50f41b4db77488795d943e977db1b237549f1d78
kiran12750 <kirandeepkaur6@my.jcu.edu.au>
2026-05-23 05:29:15 +1000

    Setup Docker and WordPress local environment

---
+++ wp-content/themes/astra-child/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+function astra_child_enqueue_styles() {
+
+    wp_enqueue_style(
+        'parent-style',
+        get_template_directory_uri() . '/style.css'
+    );
+
+}
+
+add_action('wp_enqueue_scripts', 'astra_child_enqueue_styles');
